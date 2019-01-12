1ba53ca768962dfe6b155f892082f8eb9d3f6924
Yusuf Pisan <pisan@uw.edu>
2019-01-12 07:57:45 -0800

    added --force for cpplint

--- simplecompile.sh
+++ simplecompile.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+# To more easily compile and run this program on CSS Linux Lab
+# Lines starting with '$' indicate what is typed on command line
+
 # make this file executable
-# command line: chmod 700 simplecompile.sh
+# $ chmod 700 simplecompile.sh
 # redirect the output and stderr from this file to output.txt
-# command line: ./simplecompile.sh > output.txt 2>&1
+# $ ./simplecompile.sh > output.txt 2>&1
 
 
 date
 
 echo "*** Compiling"
-g++ -std=c++14 -Wall -Wextra -Wno-sign-compare *.cpp -g -o myprogram.exe
+g++ -DUSE_CATCH -std=c++14 -Wall -Wextra -Wno-sign-compare *.cpp -g -o myprogram.exe
 
 echo "*** cpplint"
 cpplint *.cpp *.h
 
 echo "*** cppcheck"
-cppcheck --enable=all --inconclusive --language=c++ --std=posix --suppress=missingIncludeSystem *.cpp *.h
+cppcheck --enable=all --force --inconclusive --language=c++ --std=posix --suppress=missingIncludeSystem *.cpp *.h
 
 echo "*** running"
 ./myprogram.exe
